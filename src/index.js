eae8012a7602922944aecf73835df3efa97980f0
paterfamelias@gmail.com <paterfamelias@gmail.com>
2024-01-14 18:28:24 +0600

    added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/app/App';
+import App from './app/App';
 import './style/style.scss';
 
 ReactDOM.createRoot(document.getElementById('root'))
   .render(
-      <App />
+    <App/>
   )
